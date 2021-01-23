6fa9550eeb58fe671ab9106a47f11ec3da372671
Nelson <nbritob@hotmail.com>
2021-01-23 23:30:47 +0000

    Todo List final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+//import component package for components
 
+//Typescript decorator
 @Component({
   selector: 'app-root',
+  //points to html template
   templateUrl: './app.component.html',
+  //points to the stylessheet
   styleUrls: ['./app.component.css']
 })
+
+//class
 export class AppComponent {
-  title = 'todolist';
+  //title: string = 'Todo List';
+
+  //optional
+  // constructor() {
+  //   this.changeTitle('other list')
+  // }
+
+  // changeTitle(title: string):void{
+  //   this.title = title;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
+//meeting space for all the components
+
+//root-app module
 @NgModule({
   declarations: [
-    AppComponent
+    //components go here, using cli will do automatically
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
+    //modules go here
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
+  //provider will have the services used.
   providers: [],
+  //bootstrap is what we are showing on screen
   bootstrap: [AppComponent]
 })
 export class AppModule { }
